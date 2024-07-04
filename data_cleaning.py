5b7b48ef8c42931b89a5f2f1b3b1eb3692d984ba
54F-A <sarfaraj_ahmed@outlook.com>
2024-07-04 18:23:36 +0100

    Updated data cleaning methods.

--- data_cleaning.py
+++ data_cleaning.py
@@ -25,7 +25,9 @@ class DataCleaning:
         """Cleans the user data.
 
         Performs the following:
-        1.
+        1.Converts the date_of_birth & join_date columns to datetime format.
+        2. Maps country_code to country.
+        3. Formats phone number depending on country_code.
 
         Returns:
             DataFrame: Cleaned DataFrame containing processed user data.
@@ -45,6 +47,15 @@ class DataCleaning:
 
 
         def format_phone_number(phone_number, country_code):
+            """Converts product weight to kg.
+
+            Args:
+                phone_number (DataFrame): DataFrame containing product data with weight column.
+                country_code
+
+            Returns:
+                DataFrame: Cleaned DataFrame with weights converted to kg.
+            """
             phone_number = re.sub(r'\s+|\(|\)|\.|-', '', phone_number)
 
             if country_code == 'GB': 
@@ -101,40 +112,13 @@ class DataCleaning:
             DataFrame: Cleaned DataFrame containing processed card data.
         """
         df = self.data.copy()
-        
-        df['card_number'] = df['card_number'].apply(lambda x: re.sub(r'\D', '', str(x)))
-        valid_lengths = {
-            'American Express': [15],
-            'Discover': [16],
-            'Diners Club / Carte Blanche': [14],
-            'JCB 15 digit': [15],
-            'JCB 16 digit': [16],
-            'Maestro': [12, 13 ,16],
-            'Mastercard': [16],
-            'VISA 13 digit': [13],
-            'VISA 16 digit': [16],
-            'VISA 19 digit': [19]
-        }
 
-        def is_valid_length(card_number, provider):
-            """Checks if the card number length is valid.
+        valid_expiry_date = re.compile(r'^(0[1-9]|1[0-2])\/\d{2}$')
+        df = df[df['expiry_date'].apply(lambda x: bool(valid_expiry_date.match(x)))]
 
-            Args:
-                card_number (str): The card number to be validated.
-                provider (str): The card provider corresponding to the card number.
-
-            Returns:
-                bool: True if the card number length is valid for the given provider, otherwise False.
-            """
-            return len(card_number) in valid_lengths.get(provider, [])
-        
-        df = df[df.apply(lambda row: is_valid_length(row['card_number'], row['card_provider']), axis=1)]
-
-        df['expiry_date'] = pd.to_datetime(df['expiry_date'], format='%m/%y', errors='coerce')
-        df = df[df['expiry_date'] >= pd.Timestamp.now()]
-
-        df['date_payment_confirmed'] = pd.to_datetime(df['date_payment_confirmed'], errors='coerce')
-        df.dropna(subset=['date_payment_confirmed'], inplace=True)
+        df['card_number'] = df['card_number'].astype(str)
+        df = df[df['card_number'].notna()]
+        df['card_number'] = df['card_number'].str.replace('?', '')
         df.reset_index(drop=True, inplace=True)
         self.data = df
 
@@ -151,11 +135,11 @@ class DataCleaning:
         """
         df = self.data.copy()
 
-        df.dropna(subset=['longitude'], inplace=True)
-        df = df[df['longitude'].str.match(r'^\d+(\.\d+)?$')]
+        df = df[df['longitude'].astype(str).str.match(r'^\d+(\.\d+)?$|^N/A$')]
         columns_to_remove = ['lat']
         df.drop(columns=columns_to_remove, inplace=True, errors='ignore')
         df.insert(3, 'latitude', df.pop('latitude'))
+        df['staff_numbers'] = df['staff_numbers'].str.replace(r'\D', '', regex=True)
         df.reset_index(drop=True, inplace=True)
         self.data = df
 
@@ -258,17 +242,17 @@ if __name__ == "__main__":
 
     data_extractor = DataExtractor(db_connector)
 
-    """Extract and clean user data."""
-    user_data = data_extractor.read_rds_table('legacy_users')
-    cleaner = DataCleaning(user_data)
-    cleaned_user_data = cleaner.clean_user_data()
+    # """Extract and clean user data."""
+    # user_data = data_extractor.read_rds_table('legacy_users')
+    # cleaner = DataCleaning(user_data)
+    # cleaned_user_data = cleaner.clean_user_data()
 
-    """Extract and clean card data."""
-    pdf_link = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf" 
+    # """Extract and clean card data."""
+    # pdf_link = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf" 
 
-    card_data = data_extractor.retrieve_pdf_data(pdf_link)
-    cleaner = DataCleaning(card_data)
-    cleaned_card_data = cleaner.clean_card_data()
+    # card_data = data_extractor.retrieve_pdf_data(pdf_link)
+    # cleaner = DataCleaning(card_data)
+    # cleaned_card_data = cleaner.clean_card_data()
 
     """Extract and clean store data."""
     endpoint = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores"
@@ -278,7 +262,7 @@ if __name__ == "__main__":
     number_of_stores = data_extractor.list_number_of_stores(endpoint, headers)  
     stores_details = []
 
-    for store_number in range(1, 452):
+    for store_number in range(0, 452):
         stores_data = data_extractor.retrieve_store_details(str(store_number), stores_endpoint, headers)
         stores_details.append(stores_data)
 
@@ -286,52 +270,52 @@ if __name__ == "__main__":
     cleaner = DataCleaning(stores_dataframe)
     cleaned_store_data = cleaner.clean_store_data()
 
-    """Extract and clean product data."""
-    s3_address = "s3://data-handling-public/products.csv"
-    products_df = data_extractor.extract_from_s3(s3_address)
+    # """Extract and clean product data."""
+    # s3_address = "s3://data-handling-public/products.csv"
+    # products_df = data_extractor.extract_from_s3(s3_address)
 
-    product_data = data_extractor.extract_from_s3(s3_address)
-    cleaner = DataCleaning(product_data)
-    cleaned_product_data = cleaner.clean_products_data(products_df)
-    cleaned_product_data = cleaner.convert_product_weights(cleaned_product_data)
+    # product_data = data_extractor.extract_from_s3(s3_address)
+    # cleaner = DataCleaning(product_data)
+    # cleaned_product_data = cleaner.clean_products_data(products_df)
+    # cleaned_product_data = cleaner.convert_product_weights(cleaned_product_data)
 
-    """Extract and clean date time data."""
-    s3_address = "https://data-handling-public.s3.eu-west-1.amazonaws.com/date_details.json"
+    # """Extract and clean date time data."""
+    # s3_address = "https://data-handling-public.s3.eu-west-1.amazonaws.com/date_details.json"
 
-    date_time_data = data_extractor.extract_from_s3(s3_address)
-    cleaner = DataCleaning(date_time_data)
-    cleaned_date_time_data = cleaner.clean_date_times_data()
+    # date_time_data = data_extractor.extract_from_s3(s3_address)
+    # cleaner = DataCleaning(date_time_data)
+    # cleaned_date_time_data = cleaner.clean_date_times_data()
 
-    """Extract and clean orders data."""
-    AWS_RDS_db_connector = database_utils.DatabaseConnector(creds_file)
-    AWS_RDS_db_connector.init_db_engine(db_type='source')
-    data_extractor = DataExtractor(db_connector)
+    # """Extract and clean orders data."""
+    # AWS_RDS_db_connector = database_utils.DatabaseConnector(creds_file)
+    # AWS_RDS_db_connector.init_db_engine(db_type='source')
+    # data_extractor = DataExtractor(db_connector)
 
-    orders_data = data_extractor.read_rds_table("orders_table")
-    cleaner = DataCleaning(orders_data)
-    cleaned_orders_data = cleaner.clean_orders_data()
+    # orders_data = data_extractor.read_rds_table("orders_table")
+    # cleaner = DataCleaning(orders_data)
+    # cleaned_orders_data = cleaner.clean_orders_data()
 
     """Upload all tables to the local database."""
     local_db_connector = database_utils.DatabaseConnector(creds_file)
     local_db_connector.init_db_engine(db_type='local')
 
-    upload_table = "dim_users"
-    local_db_connector.upload_to_db(cleaned_user_data, upload_table, db_type='local')
+    # upload_table = "dim_users"
+    # local_db_connector.upload_to_db(cleaned_user_data, upload_table, db_type='local')
 
-    card_upload_table = "dim_card_details"
-    local_db_connector.upload_to_db(cleaned_card_data, card_upload_table, db_type='local')
+    # card_upload_table = "dim_card_details"
+    # local_db_connector.upload_to_db(cleaned_card_data, card_upload_table, db_type='local')
 
     store_upload_table = "dim_store_details"
     local_db_connector.upload_to_db(cleaned_store_data, store_upload_table, db_type='local')
 
-    products_upload_table = "dim_products"
-    local_db_connector.upload_to_db(cleaned_product_data, products_upload_table, db_type='local')
+    # products_upload_table = "dim_products"
+    # local_db_connector.upload_to_db(cleaned_product_data, products_upload_table, db_type='local')
 
-    date_times_upload_table = "dim_date_times"
-    local_db_connector.upload_to_db(cleaned_date_time_data, date_times_upload_table, db_type='local')
+    # date_times_upload_table = "dim_date_times"
+    # local_db_connector.upload_to_db(cleaned_date_time_data, date_times_upload_table, db_type='local')
 
-    orders_upload_table = "orders_table"
-    local_db_connector.upload_to_db(cleaned_orders_data, orders_upload_table, db_type='local')
+    # orders_upload_table = "orders_table"
+    # local_db_connector.upload_to_db(cleaned_orders_data, orders_upload_table, db_type='local')
 
     print("Tables in the local database:")
     local_db_connector.list_db_tables()
