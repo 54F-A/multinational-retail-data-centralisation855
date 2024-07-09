376c496df04e5e65aac9191f77863db6bd137c92
54F-A <sarfaraj_ahmed@outlook.com>
2024-07-09 17:52:36 +0100

    Updated data cleaning methods.

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,3 @@
-from data_extraction import DataExtractor
-import database_utils
-import datetime
 import pandas as pd
 import re
 import uuid
@@ -25,17 +22,15 @@ class DataCleaning:
         """Cleans the user data.
 
         Performs the following:
-        1.Converts the date_of_birth & join_date columns to datetime format.
-        2. Maps country_code to country.
-        3. Formats phone number depending on country_code.
+        1. Maps country to country_code.
+        2. Converts date_of_birth values to the foramt: 'YYYY-MM-DD'
+        3. Converts join_date values to the foramt: 'YYYY-MM-DD'
+        4. Removes whitespace, dots, and hyphens from the phone_number column.
 
         Returns:
             DataFrame: Cleaned DataFrame containing processed user data.
         """
         df = self.data.copy()
-
-        df['date_of_birth'] = pd.to_datetime(df['date_of_birth'], errors='coerce')
-        df['join_date'] = pd.to_datetime(df['join_date'], errors='coerce')
 
         country_mapping = {
             'Germany': 'DE',
@@ -45,58 +40,22 @@ class DataCleaning:
         df['country_code'] = df['country'].map(country_mapping)
         df = df[df['country_code'].notna()]
 
+        df['date_of_birth'] = df['date_of_birth'].apply(lambda dob: 
+            pd.to_datetime(dob) if pd.notnull(dob) else dob)
+        df = df.dropna(subset=['date_of_birth'])
+        df['date_of_birth'] = df['date_of_birth'].apply(lambda dob: 
+            dob.strftime('%Y-%m-%d') if isinstance(dob, pd.Timestamp) else dob)
 
-        def format_phone_number(phone_number, country_code):
-            """Converts product weight to kg.
+        df['join_date'] = df['join_date'].apply(lambda jd: 
+            pd.to_datetime(jd) if pd.notnull(jd) else jd)
+        df = df.dropna(subset=['join_date'])
+        df['join_date'] = df['join_date'].apply(lambda jd: 
+            jd.strftime('%Y-%m-%d') if isinstance(jd, pd.Timestamp) else jd)
 
-            Args:
-                phone_number (DataFrame): DataFrame containing product data with weight column.
-                country_code
-
-            Returns:
-                DataFrame: Cleaned DataFrame with weights converted to kg.
-            """
-            phone_number = re.sub(r'\s+|\(|\)|\.|-', '', phone_number)
-
-            if country_code == 'GB': 
-                if phone_number.startswith('('):
-                    return f"({phone_number[1:6]} {phone_number[6:9]} {phone_number[9:]}"
-                elif phone_number.startswith('0'):
-                    return f"{phone_number[0:5]} {phone_number[5:]}"
-                elif phone_number.startswith('+44'):
-                    if len(phone_number) == 13:
-                        return f"{phone_number[0:3]} {phone_number[3:7]} {phone_number[7:]}"
-                    elif len(phone_number) == 14:
-                        phone_number = phone_number.replace('+44', '')
-                        return f"{phone_number[0:3]} {phone_number[3:7]} {phone_number[7:]}"
-                else:
-                    return phone_number
-            
-            elif country_code == 'US':
-                if 'x' in phone_number:
-                    return f"{phone_number[0:3]} {phone_number[3:6]} {phone_number[6:10]} {phone_number[10:]}"
-                elif phone_number.startswith('+'):
-                    if len(phone_number) > 10:
-                        return f"{phone_number[0:3]} {phone_number[3:6]} {phone_number[6:10]} {phone_number[10:]}"
-                elif phone_number.startswith('00'):
-                    phone_number = phone_number.replace('00', '', 1)
-                    return f"+{phone_number[0:1]} {phone_number[1:5]} {phone_number[5:8]} {phone_number[8:]}"
-                else:
-                    return phone_number
-                
-            elif country_code == 'DE':
-                if phone_number.startswith('+49'):
-                    return f"{phone_number[0:3]} {phone_number[3:5]} {phone_number[5:9]} {phone_number[9:]}"
-                elif phone_number.startswith('0'):
-                    return f"{phone_number[0:4]} {phone_number[4:]}"
-                else:
-                    return phone_number
-            
-            else:
-                return phone_number
-
+        df['phone_number'] = df['phone_number'].apply(lambda number:
+            re.sub(r'\s+|\.|-', '', number) if pd.notnull(number) else number)
         df = df[df['phone_number'].notna()]
-        df['phone_number'] = df.apply(lambda row: format_phone_number(row['phone_number'], row['country_code']), axis=1)
+
         df.reset_index(drop=True, inplace=True)
         self.data = df
         
@@ -106,19 +65,27 @@ class DataCleaning:
         """Cleans the card data.
 
         Performs the following:
-        1.
+        1. Removes expiry_date values that are not in the format: 'MM/YY'
+        2. Removes '?' in the card_number column.
+        3. Converts date_payment_confirmed values to the foramt: 'YYYY-MM-DD'
 
         Returns:
             DataFrame: Cleaned DataFrame containing processed card data.
         """
         df = self.data.copy()
-
+        
         valid_expiry_date = re.compile(r'^(0[1-9]|1[0-2])\/\d{2}$')
         df = df[df['expiry_date'].apply(lambda x: bool(valid_expiry_date.match(x)))]
 
         df['card_number'] = df['card_number'].astype(str)
-        df = df[df['card_number'].notna()]
         df['card_number'] = df['card_number'].str.replace('?', '')
+
+        df['date_payment_confirmed'] = df['date_payment_confirmed'].apply(lambda dpc: 
+            pd.to_datetime(dpc) if pd.notnull(dpc) else dpc)
+        df = df.dropna(subset=['date_payment_confirmed'])
+        df['date_payment_confirmed'] = df['date_payment_confirmed'].apply(lambda dpc: 
+            dpc.strftime('%Y-%m-%d') if isinstance(dpc, pd.Timestamp) else dpc)
+
         df.reset_index(drop=True, inplace=True)
         self.data = df
 
@@ -128,7 +95,10 @@ class DataCleaning:
         """Cleans the store data.
 
         Performs the following:
-        1.
+        1. Removes values from the longitude column that are neither floats nor 'N/A'.
+        2. Removes 'lat' column.
+        3. Moves latitude column to the third position in the table.
+        4. Removes staff_numbers values that are non-numerical.
 
         Returns:
             DataFrame: Cleaned DataFrame containing processed store data.
@@ -140,21 +110,22 @@ class DataCleaning:
         df.drop(columns=columns_to_remove, inplace=True, errors='ignore')
         df.insert(3, 'latitude', df.pop('latitude'))
         df['staff_numbers'] = df['staff_numbers'].str.replace(r'\D', '', regex=True)
+
         df.reset_index(drop=True, inplace=True)
         self.data = df
 
         return df
+    
+    @staticmethod
+    def convert_weight(weight):
+            """Converts product weight to kg.
 
-    def convert_product_weights(self, products_df):
-        """Converts product weight to kg.
+            Args:
+                weight (str): Weight of the product as a string.
 
-        Args:
-            products_df (DataFrame): DataFrame containing product data with weight column.
-
-        Returns:
-            DataFrame: Cleaned DataFrame with weights converted to kg.
-        """
-        def convert_weight(weight):
+            Returns:
+                str: Weight converted to kg as a string.
+            """
             if isinstance(weight, str):
                 weight = weight.lower().strip()
                 conversions = {
@@ -166,14 +137,35 @@ class DataCleaning:
                     if unit in weight:
                         numeric_value = float(re.sub(r'[^\d.]', '', weight))
                         return f"{round(numeric_value * factor, 3)} kg"
-        
+
+    def convert_product_weights(self, products_df):
+        """Converts product weight to kg.
+
+        Performs the following:
+        1. Converts weight values from g to kg.
+
+        Args:
+            products_df (DataFrame): DataFrame containing product data.
+
+        Returns:
+            DataFrame: Cleaned DataFrame with weights converted to kg.
+        """
+        df = products_df.copy()
+
+        convert_weight = DataCleaning.convert_weight
         products_df['weight'] = products_df['weight'].apply(convert_weight)
+
         products_df.reset_index(drop=True, inplace=True)
+        self.data = df
 
         return products_df
     
     def clean_products_data(self, products_df):
         """Cleans the product data.
+
+        Performs the following:
+        1. Drops missing values.
+        2. Filters rows where removed column is either 'Still_avaliable' or 'Removed'.
 
         Args:
             products_df (DataFrame): DataFrame containing product data.
@@ -185,17 +177,35 @@ class DataCleaning:
 
         df.dropna(inplace=True)
         df = df.loc[df['removed'].isin(['Still_avaliable', 'Removed'])]
-        df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)       
+        df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)    
+
         df.reset_index(drop=True, inplace=True)
         self.data = df
 
         return df
     
+    @staticmethod
+    def is_valid_uuid(value):
+            """Checks if the value is a valid UUID.
+
+            Args:
+                value (str): The value to be checked.
+
+            Returns:
+                bool: True if the value is a valid UUID.
+            """
+            try:
+                uuid.UUID(str(value))
+                return True
+            except ValueError:
+                return False
+
     def clean_date_times_data(self):
         """Cleans the date details data.
 
         Performs the following:
         1. Drops rows with missing values.
+        2. Filters rows with valid UUIDs in the date_uuid column.
 
         Returns:
             DataFrame: Cleaned DataFrame containing processed date details data.
@@ -203,15 +213,9 @@ class DataCleaning:
         df = self.data.copy()
 
         df.dropna(inplace=True) 
-
-        def is_valid_uuid(val):
-            try:
-                uuid.UUID(str(val))
-                return True
-            except ValueError:
-                return False
-        
+        is_valid_uuid = DataCleaning.is_valid_uuid
         df = df[df['date_uuid'].apply(is_valid_uuid)]
+
         df.reset_index(drop=True, inplace=True)
         self.data = df
 
@@ -234,89 +238,4 @@ class DataCleaning:
         self.data = df
 
         return df
-        
-if __name__ == "__main__":
-    creds_file = r'c:/Users/safi-/OneDrive/Occupation/AiCore/AiCore Training/PROJECTS/' \
-                                  'Multinational Retail Data Centralisation Project/multinational-retail-data-centralisation855/db_creds.yaml'
-    db_connector = database_utils.DatabaseConnector(creds_file)
-
-    data_extractor = DataExtractor(db_connector)
-
-    """Extract and clean user data."""
-    user_data = data_extractor.read_rds_table('legacy_users')
-    cleaner = DataCleaning(user_data)
-    cleaned_user_data = cleaner.clean_user_data()
-
-    """Extract and clean card data."""
-    pdf_link = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf" 
-
-    card_data = data_extractor.retrieve_pdf_data(pdf_link)
-    cleaner = DataCleaning(card_data)
-    cleaned_card_data = cleaner.clean_card_data()
-
-    """Extract and clean store data."""
-    endpoint = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores"
-    stores_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}'
-    headers = {"x-api-key": "yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX"}
-    
-    number_of_stores = data_extractor.list_number_of_stores(endpoint, headers)  
-    stores_details = []
-
-    for store_number in range(0, 452):
-        stores_data = data_extractor.retrieve_store_details(str(store_number), stores_endpoint, headers)
-        stores_details.append(stores_data)
-
-    stores_dataframe = pd.concat(stores_details, ignore_index=True)
-    cleaner = DataCleaning(stores_dataframe)
-    cleaned_store_data = cleaner.clean_store_data()
-
-    """Extract and clean product data."""
-    s3_address = "s3://data-handling-public/products.csv"
-    products_df = data_extractor.extract_from_s3(s3_address)
-
-    product_data = data_extractor.extract_from_s3(s3_address)
-    cleaner = DataCleaning(product_data)
-    cleaned_product_data = cleaner.clean_products_data(products_df)
-    cleaned_product_data = cleaner.convert_product_weights(cleaned_product_data)
-
-    """Extract and clean date time data."""
-    s3_address = "https://data-handling-public.s3.eu-west-1.amazonaws.com/date_details.json"
-
-    date_time_data = data_extractor.extract_from_s3(s3_address)
-    cleaner = DataCleaning(date_time_data)
-    cleaned_date_time_data = cleaner.clean_date_times_data()
-
-    """Extract and clean orders data."""
-    AWS_RDS_db_connector = database_utils.DatabaseConnector(creds_file)
-    AWS_RDS_db_connector.init_db_engine(db_type='source')
-    data_extractor = DataExtractor(db_connector)
-
-    orders_data = data_extractor.read_rds_table("orders_table")
-    cleaner = DataCleaning(orders_data)
-    cleaned_orders_data = cleaner.clean_orders_data()
-
-    """Upload all tables to the local database."""
-    local_db_connector = database_utils.DatabaseConnector(creds_file)
-    local_db_connector.init_db_engine(db_type='local')
-
-    upload_table = "dim_users"
-    local_db_connector.upload_to_db(cleaned_user_data, upload_table, db_type='local')
-
-    card_upload_table = "dim_card_details"
-    local_db_connector.upload_to_db(cleaned_card_data, card_upload_table, db_type='local')
-
-    store_upload_table = "dim_store_details"
-    local_db_connector.upload_to_db(cleaned_store_data, store_upload_table, db_type='local')
-
-    products_upload_table = "dim_products"
-    local_db_connector.upload_to_db(cleaned_product_data, products_upload_table, db_type='local')
-
-    date_times_upload_table = "dim_date_times"
-    local_db_connector.upload_to_db(cleaned_date_time_data, date_times_upload_table, db_type='local')
-
-    orders_upload_table = "orders_table"
-    local_db_connector.upload_to_db(cleaned_orders_data, orders_upload_table, db_type='local')
-
-    print("Tables in the local database:")
-    local_db_connector.list_db_tables()
     
